778729c44207768d7cb08a8f2bb9b23f34bb514e
V1staz <2353901@tongji.edu.cn>
2026-04-21 20:15:39 +0800

    matplot

--- main.py
+++ main.py
@@ -245,4 +245,4 @@ async def websocket_endpoint(websocket: WebSocket, user_id: UUID, session_id: UU
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
